89dca3e5658efaf1ac7439708abcb747e76db0f7
paperplane110 <1374736640@qq.com>
2024-08-13 23:32:20 +0800

    progress 5:53:19

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,8 +1,42 @@
+"use client"
 
-const DocumentIdPage = () => {
+import { useQuery } from "convex/react";
+
+import { Toolbar } from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">
+  }
+}
+
+const DocumentIdPage = ({
+  params
+}: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, { id: params.documentId })
+
+  if (document === undefined) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
+
+  if (document === null) {
+    return (
+      <div>Not Found</div>
+    )
+  }
+
   return (
-    <div>
-      DocumentId
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
     </div>
   )
 }
